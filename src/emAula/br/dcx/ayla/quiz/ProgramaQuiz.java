cb8e6687d31241b8bcea53978709ee03d0660583
tercio.oliveira <fracelino2011@gmail.com>
2024-02-02 13:02:15 -0300

    humm things

--- src/emAula/br/dcx/ayla/quiz/ProgramaQuiz.java
+++ src/emAula/br/dcx/ayla/quiz/ProgramaQuiz.java
@@ -5,9 +5,10 @@ import java.util.List;
 import java.util.Scanner;
 
 public class ProgramaQuiz {
+    private static List<PerguntaME> perguntas = new LinkedList<>();
+    private static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        List<PerguntaME> perguntas = new LinkedList<>();
-        Scanner sc = new Scanner(System.in);
         boolean continuar = true;
         while (continuar) {
             System.out.println("Digite uma opção: \n1.Cadastrar pergunta \n2.Responder quiz");
@@ -33,13 +34,42 @@ public class ProgramaQuiz {
                 System.out.println("Pergunta cadastrada com sucesso");
             } else if (opcao.equals("2")) {
                 if (perguntas.size()== 0) {
-                    System.out.println("Nenhuma pergunta foi cadastrada");
-                    //TODO: fazer.
+                    System.out.println("Nenhuma pergunta foi cadastrada!");
+                    continue;
+                } else if (perguntas.size() >= 1) {
+                    for(PerguntaME s: perguntas) {
+                        int numero = 0;
+                        System.out.println( numero + "" + s.getEnunciado());
+                        numero++;
+                    }
+                    int input = Integer.parseInt(sc.nextLine());
+                    quiz(perguntas.get(input));
                 }
             } else if (opcao.equals("3")) {
-                continuar = false
+                continuar = false;
             }
         }
         sc.close();
+    }
+    public static void quiz(PerguntaME quiz) {
+        System.out.println(quiz.getEnunciado());
+        int quantAlternativas = quiz.getAlternativas().length;
+        for (int i = 0; i < quantAlternativas; i++) {
+            System.out.println(i + "" + quiz.getAlternativas()[i]);
+        }
+        int input = Integer.parseInt(sc.nextLine());
+        while (true){
+            System.out.println();
+            if (input > quantAlternativas || input < quantAlternativas) {
+                System.out.println("Essa alternativa não exister");
+                continue;
+            } else {
+                if (quiz.getAlternativas()[input].equals(quiz.getRespostaCorreta())) {
+                    System.out.println("Parabens, você acertou");
+                }else {
+                    System.out.println("Errada!");
+                }
+            }
+        }
     }
 }
