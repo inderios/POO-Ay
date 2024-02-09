2872886901f339c807d673517ed30675bbf67e82
tercio.oliveira <fracelino2011@gmail.com>
2024-02-08 21:42:08 -0300

    just starting

--- src/atividade01/PerguntaVF.java
+++ src/atividade01/PerguntaVF.java
@@ -1,6 +1,13 @@
 package atividade01;
 
 public class PerguntaVF extends Pergunta {
+    private String afirmativa;
+    public PerguntaVF (String enunciado, String afirmativa, String respostaCorreta) {
+
+    }
+    public PerguntaVF() {
+
+    }
     @Override
     public boolean estahCorretaResposta(String resposta) {
         //TODO: IMPLEMENTAR
